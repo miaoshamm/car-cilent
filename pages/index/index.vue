92bfa0b6508bfca0a08ff04c5bfc9070d182037e
heiheihei-ctrl <208733909@qq.com>
2023-12-05 10:40:14 +0800

    feat: 修改

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -96,42 +96,13 @@
 				</view>
 			</view>
 		</view>
-		<u-tabbar style="height: 128rpx;border-top: 4px solid #eee;" :border="false" activeColor="#DFA0B1" :value="value1"
-			:fixed="true" :placeholder="true" :safeAreaInsetBottom="false">
-			<u-tabbar-item text="首页" @click="click1">
-				<template #active-icon>
-					<image style="width: 44rpx;height: 40rpx;margin: 0 0 6rpx" src="/static/images/tabbar/index-active.png">
-					</image>
-				</template>
-				<template #inactive-icon>
-					<image style="width: 44rpx;height: 40rpx;margin: 0 0 6rpx" src="/static/images/tabbar/index.png"></image>
-				</template>
-			</u-tabbar-item>
-			<u-tabbar-item text="我的订单" @click="click1">
-				<template #active-icon>
-					<image style="width: 36rpx;height: 40rpx;margin:0 0 6rpx" src="/static/images/tabbar/orders-active.png">
-					</image>
-				</template>
-				<template #inactive-icon>
-					<image style="width: 36rpx;height: 40rpx;margin:0 0 6rpx" src="/static/images/tabbar/orders.png"></image>
-				</template>
-			</u-tabbar-item>
-			<u-tabbar-item text="个人中心" @click="click1">
-				<template #active-icon>
-					<image style="width: 32rpx;height: 42rpx;margin:0 0 6rpx" src="/static/images/tabbar/account-active.png">
-					</image>
-				</template>
-				<template #inactive-icon>
-					<image style="width: 32rpx;height: 42rpx;margin:0 0 6rpx" src="/static/images/tabbar/account.png"></image>
-				</template>
-			</u-tabbar-item>
-		</u-tabbar>
+		<Tabbar :value="0"/>
 	</view>
 </template>
 
 <script setup>
 	import { ref } from 'vue';
-	const value1 = ref(0)
+	import Tabbar from "@/components/tabbar/tabbar.vue"
 	const banner = ref([
 		'https://cdn.uviewui.com/uview/swiper/swiper1.png',
 		'https://cdn.uviewui.com/uview/swiper/swiper2.png',
@@ -219,19 +190,6 @@
 	
 	const bannerClick = () => {
 
-	}
-	const click1 = (index) => {
-		let url = ""
-		if (index === 0) {
-			url = "/pages/index/index"
-		} else if (index === 1) {
-			url = "/pages/orders/orders"
-		} else {
-			url = "/pages/account/account"
-		}
-		uni.switchTab({
-			url
-		})
 	}
 </script>
 
